bb80686ffc27c40d4df652bf16a444204f107942
Luis guilherme <gui.cruz9090@gmail.com>
2026-04-15 14:41:10 -0300

    fix: usando preset oficial nitro e pasta .output

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
-    cloudflare({
-      viteEnvironment: { name: "ssr" }
-    }),
     tanstackStart(),
   ],
 });
